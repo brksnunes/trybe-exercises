b1144b4a5dfb39182cc77e8c7973df985b8f3265
Rodrigo Nunes <brksnunes@protonmail.com>
2022-05-15 14:05:19 -0300

    Added JS exercise

--- 01-fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -207,3 +207,75 @@ if (numero1 % 2 !== 0 || numero2 % 2 !== 0 || numero3 % 2 !== 0 ){
 }
 
 console.log(impar);
+
+/*
+10. Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+- Atente que, sobre o custo do produto, incide um imposto de 20%.
+- Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+    - valorCustoTotal = valorCusto + impostoSobreOCusto
+    - lucro = valorVenda - valorCustoTotal (lucro de um produto)
+*/
+const custo = 100;
+const valorVenda = 155;
+
+let custoTotal = custo + (custo * 0.20);
+let totalGeral;
+// console.log(custoTotal);
+
+if (custo >= 0 && valorVenda >= 0 && custoTotal >= 0) {
+  totalGeral = (valorVenda - custoTotal) * 1000;
+  console.log("O total geral é: ", totalGeral);
+} else {
+  console.log("Valor inválido");
+}
+
+/*
+11. Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+    INSS (Instituto Nacional do Seguro Social)
+        Salário bruto até R$ 1.556,94: alíquota de 8%
+        Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+        Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+        Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+    IR (Imposto de Renda)
+        Até R$ 1.903,98: isento de imposto de renda
+        De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+        De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+        De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+        Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+*/
+//########## RESOLUÇÃO DO GABARITO DO COURSE ##########
+
+let aliquotINSS;
+let aliquotIR;
+
+const grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+const baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
